89b41834d4817cdae1aaa43dfed50e1978ad0cfe
brandondeveloper <brandonochieng72@gmail.com>
2025-02-06 12:42:52 +0300

    Removed additional unnecessary modules

--- product_management/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    specifications = models.TextField()
-    quantity_in_stock = models.PositiveIntegerField()
